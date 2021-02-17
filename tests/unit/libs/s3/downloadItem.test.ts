074f9be825ea4c8b0c9d79503875f3bdfeb6facc
Mauricio Valdvia <movaldivia@uc.cl>
2021-02-17 15:23:30 -0300

    chore: comment test

--- tests/unit/libs/s3/downloadItem.test.ts
+++ tests/unit/libs/s3/downloadItem.test.ts
@@ -1,34 +1,34 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
-import { S3 } from 'aws-sdk';
-import { downloadItem } from '../../../../lib/s3/downloadItem';
-import { expect, sinon } from '../../../libs.tests/chai.commons';
-import { DownloadS3ParamsFactory } from '../../../factories/s3.factory';
+// import { S3 } from 'aws-sdk';
+// import { downloadItem } from '../../../../lib/s3/downloadItem';
+// import { expect, sinon } from '../../../libs.tests/chai.commons';
+// import { DownloadS3ParamsFactory } from '../../../factories/s3.factory';
 
-describe.only('AWS-WRAPPER: downloadItem', () => {
-  let s3Stub: sinon.SinonStub<any, any>;
+// describe.only('AWS-WRAPPER: downloadItem', () => {
+//   let s3Stub: sinon.SinonStub<any, any>;
 
-  beforeEach(() => {
-    s3Stub = sinon.stub(S3.prototype, 'getObject');
-  });
+//   beforeEach(() => {
+//     s3Stub = sinon.stub(S3.prototype, 'getObject');
+//   });
 
-  afterEach(() => {
-    s3Stub.restore();
-  });
+//   afterEach(() => {
+//     s3Stub.restore();
+//   });
 
-  it('[SUCCESS] should return the upload Item', async () => {
-    const downloadItemParams = DownloadS3ParamsFactory();
-    s3Stub.returns({
-      promise: () => {
-        return {
-          Body: Buffer.from('dummy'),
-        };
-      },
-    });
-    const values = (await downloadItem(
-      downloadItemParams,
-    )) as S3.GetObjectOutput;
-    const text = values.Body?.toString();
-    expect(s3Stub).to.have.been.calledOnce;
-    expect(text).to.be.equal('dummy');
-  });
-});
+//   it('[SUCCESS] should return the upload Item', async () => {
+//     const downloadItemParams = DownloadS3ParamsFactory();
+//     s3Stub.returns({
+//       promise: () => {
+//         return {
+//           Body: Buffer.from('dummy'),
+//         };
+//       },
+//     });
+//     const values = (await downloadItem(
+//       downloadItemParams,
+//     )) as S3.GetObjectOutput;
+//     const text = values.Body?.toString();
+//     expect(s3Stub).to.have.been.calledOnce;
+//     expect(text).to.be.equal('dummy');
+//   });
+// });

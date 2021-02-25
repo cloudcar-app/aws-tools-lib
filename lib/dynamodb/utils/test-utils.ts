cf8bb1a153098a4b2da49f4770d4453c8aaddd91
Pablo <pdecarcer@abstract.cl>
2021-02-25 19:10:13 -0300

    fix: method

--- lib/dynamodb/utils/test-utils.ts
+++ lib/dynamodb/utils/test-utils.ts
@@ -5,11 +5,11 @@ const documentClient = new DynamoDB.DocumentClient({ service: dynamo });
 // eslint-disable-next-line import/prefer-default-export
 export const emptyTable = async (
   tableName: string,
-  attributesToGet: [string],
+  attributesToGet: string[],
 ) => {
   const params = {
     TableName: tableName,
-    AttributesToGet: attributesToGet,
+    ProjectionExpression: `${attributesToGet.toString()}`,
   };
   const result = await documentClient.scan(params).promise();
   result.Items?.forEach(async (item) => {

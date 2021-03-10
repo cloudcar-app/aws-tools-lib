1490ec557648a10824b1aa7a4da46e2e1b61828c
Mauricio Valdvia <movaldivia@uc.cl>
2021-03-10 16:18:35 -0300

    feat: query Items

--- lib/dynamodb/index.ts
+++ lib/dynamodb/index.ts
@@ -1,5 +1,5 @@
 export { createItem } from './createItem';
-export { queryAtLeastOneItem } from './queryItems';
+export { queryAtLeastOneItem } from './queryAtLeastOneItem';
 export { getItem } from './getItem';
 export { listItems } from './listItems';
 export { updateItem } from './updateItem';

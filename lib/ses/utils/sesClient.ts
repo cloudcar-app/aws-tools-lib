c34cd19799fd80dd190503486e6e97f69d9e2bf6
bawas <tvharris@uc.cl>
2021-03-24 00:22:05 -0300

    fix: add local ses client

--- lib/ses/utils/sesClient.ts
+++ lib/ses/utils/sesClient.ts
@@ -1,7 +1,7 @@
 import { SES } from 'aws-sdk';
 
 export const ses = process.env.LOCAL
-  ? new SES()
+  ? new SES({ region: 'us-east-1', endpoint: 'http://localhost:9001' })
   : new SES({
       region: process.env.REGION || 'us-east-1',
     });

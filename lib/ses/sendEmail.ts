4a1c0da69bce783314808b46ca349c0801a5d6fc
bawas <tvharris@uc.cl>
2021-03-24 00:06:34 -0300

    feat: add description

--- lib/ses/sendEmail.ts
+++ lib/ses/sendEmail.ts
@@ -1,8 +1,12 @@
+/* eslint-disable max-len */
 import { ses } from './utils/sesClient';
 import MessageError from './utils/message.errors';
 import { SendEmailSESparams } from './types';
 import CloudcarError from '../errors/index';
 
+/**
+ * return the email message id identifier. The message may not include more than 50 recipients, across the To:, CC: and BCC: fields. If you need to send an email message to a larger audience, you can divide your recipient list into groups of 50 or fewer, and then call the SendEmail operation several times to send the message to each group
+ */
 export const sendEmail = async (params: SendEmailSESparams) => {
   const { to, from, subject, text } = params;
 

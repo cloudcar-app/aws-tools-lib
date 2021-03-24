cf71c733535cd87b7a90b70a8074b527baa916b8
bawas <tvharris@uc.cl>
2021-03-23 22:19:10 -0300

    fix: factory text

--- tests/factories/ses.factory.ts
+++ tests/factories/ses.factory.ts
@@ -5,5 +5,5 @@ export const SendEmailSESParamsFactory = define<SendEmailSESparams>({
   from: 'email@example.com',
   to: ['email@example.com'],
   subject: 'Bienvenido a la administración de Cloudcar',
-  text: 'has sido invidatado a la plataforma de administración',
+  text: 'has sido invitado a la plataforma de administración',
 });

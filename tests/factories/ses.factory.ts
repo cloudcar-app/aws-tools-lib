47019935fa7b78123f43479de20ca367fe2ebe7e
bawas <tvharris@uc.cl>
2021-03-30 17:14:15 -0300

    test: add ses unit test

--- tests/factories/ses.factory.ts
+++ tests/factories/ses.factory.ts
@@ -1,9 +1,19 @@
 import { define } from 'cooky-cutter';
-import { SendEmailSESparams } from '../../lib/ses/types';
+import {
+  SendEmailSESparams,
+  SendEmailTemplatedSESparams,
+} from '../../lib/ses/types';
 
 export const SendEmailSESParamsFactory = define<SendEmailSESparams>({
   from: 'email@example.com',
   to: ['email@example.com'],
   subject: 'Bienvenido a la administración de Cloudcar',
   text: 'has sido invitado a la plataforma de administración',
+});
+
+export const SendTemplatedEmailSESParamsFactory = define<SendEmailTemplatedSESparams>({
+  from: 'email@example.com',
+  to: ['email@example.com'],
+  template: 'template name',
+  templateData: '{}',
 });

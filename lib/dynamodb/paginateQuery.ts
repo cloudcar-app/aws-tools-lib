3765bb94a3c3413bf8783f4cca125fee66ea77c1
Mauricio Valdvia <movaldivia@uc.cl>
2021-05-13 12:59:15 -0400

    feat: add paginateQuery

---
+++ lib/dynamodb/paginateQuery.ts
@@ -0,0 +1,46 @@
+import { DynamoDB } from 'aws-sdk';
+
+import { documentClient } from './utils/dynamoClient';
+
+export const paginateQuery = async <T>({
+  params,
+  pageSize,
+  acc = [],
+}: {
+  params: DynamoDB.DocumentClient.QueryInput;
+  pageSize: number;
+  acc?: T[];
+}): Promise<{ page: T[]; hasNextPage: boolean }> => {
+  const remaining = pageSize - acc.length;
+  const result = await documentClient.query(params).promise();
+  const newItems = result.Items || [];
+  const newAcc = [...acc, ...(newItems.slice(0, remaining) as T[])];
+
+  const notMoreItemsToQuery: boolean = !result.LastEvaluatedKey;
+
+  if (notMoreItemsToQuery) {
+    return {
+      page: newAcc,
+      hasNextPage: newItems.length > remaining,
+    };
+  }
+
+  const requestedPageSizeNotFilled: boolean = newAcc.length < pageSize;
+  const checkIfExistOneMorePage: boolean = newItems.length <= remaining;
+
+  if (requestedPageSizeNotFilled || checkIfExistOneMorePage) {
+    return paginateQuery({
+      params: {
+        ...params,
+        ExclusiveStartKey: result.LastEvaluatedKey,
+      },
+      pageSize,
+      acc: newAcc,
+    });
+  }
+
+  return {
+    page: newAcc,
+    hasNextPage: true,
+  };
+};

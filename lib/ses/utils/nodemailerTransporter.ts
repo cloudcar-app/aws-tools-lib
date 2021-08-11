5377a6e0f03b826d40e9dffa3e13fa266766ca7e
bawas <tvharris@uc.cl>
2021-08-11 17:39:17 -0400

    fix: add configuration set to transporter

--- lib/ses/utils/nodemailerTransporter.ts
+++ lib/ses/utils/nodemailerTransporter.ts
@@ -1,6 +1,12 @@
 import { createTransport } from 'nodemailer';
 import { ses } from './sesClient';
 
+ses.createConfigurationSet({
+  ConfigurationSet: {
+    Name: 'templateFailure',
+  },
+});
+
 export const nodemailerTransporter = createTransport({
   SES: ses,
 });

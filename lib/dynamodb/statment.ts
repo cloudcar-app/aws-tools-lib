6bdf8b46f6c7d58909c1d41ef67d9579439cab8d
pdecarcer <298465+pdecarcer@users.noreply.github.com>
2021-09-14 23:04:47 -0300

    Update statment.ts

--- lib/dynamodb/statment.ts
+++ lib/dynamodb/statment.ts
@@ -1,7 +1,7 @@
 import { dynamo } from './utils/dynamoClient';
 /**
  * This method is when you need to make PartiQl Query
- * @param query You need a PartiQl statement to query
+ * @param query The query statement in PartiQL
  * @returns result
  */
 export const statement = async (query: string): Promise<Object> => {
